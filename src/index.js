a3bc50e98a7c80714387df22437f6ea516adaa0a
Uladzislau Pauliushchyk <plusch321@mail.ru>
2019-03-27 19:11:15 +0300

    fix: made for explanation

--- src/index.js
+++ src/index.js
@@ -1,42 +1,13 @@
-module.exports = function solveSudoku(sudoku) {
-	let i = 0, j = 0;
-	function check() {
-		let arr = [];
-		for (let k = 0; k < 9; k++) {
-			let kJ = Math.trunc(j / 3) * 3 + Math.trunc(k / 3);
-			let kI = Math.trunc(i / 3) * 3 + (k % 3);
-			arr.push(sudoku[j][k], sudoku[k][i], sudoku[kJ][kI]);
-		}
-		return [1, 2, 3, 4, 5, 6, 7, 8, 9].filter(elem => !arr.includes(elem));
-		// let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-		// for (let k = 0; k < 9; k++) {
-		// 	let kJ = Math.trunc(j / 3) * 3 + Math.trunc(k / 3);
-		// 	let kI = Math.trunc(i / 3) * 3 + (k % 3);
-		// 	if (sudoku[j][k] && arr.includes(sudoku[j][k])) arr.splice(arr.indexOf(sudoku[j][k]), 1);//столбец
-		// 	if (sudoku[k][i] && arr.includes(sudoku[k][i])) arr.splice(arr.indexOf(sudoku[k][i]), 1);//строка
-		// 	if (sudoku[kJ][kI] && arr.includes(sudoku[kJ][kI])) arr.splice(arr.indexOf(sudoku[kJ][kI]), 1);
-		// }
-		// return arr;
+module.exports = function solveSudoku(sudoku, col = 0, row = 0, impsNum = []) {
+	if (col > 8) col = 0, row++;
+	if (row > 8) return sudoku;
+	if (sudoku[row][col]) return solveSudoku(sudoku, col + 1, row) ? sudoku : false;
+	for (let k = 0; k < 9; k++) {
+		let kRow = Math.trunc(row / 3) * 3 + Math.trunc(k / 3);
+		let kCol = Math.trunc(col / 3) * 3 + (k % 3);
+		impsNum.push(sudoku[row][k], sudoku[k][col], sudoku[kRow][kCol]);
 	}
-	function plusch(sudoku) {
-		if (i > 8) i = 0, j++;
-		if (j > 8) return sudoku;
-		//если элемент был	
-		if (sudoku[j][i]) {
-			i++;
-			if (plusch(sudoku)) return sudoku;
-			if (--i < 0) i = 8, j--;
-			return 0;
-		}
-		//если элемент пустой
-		let posNum = check();
-		for (let c = 0; c < posNum.length; c++) {
-			sudoku[j][i++] = posNum[c];
-			if (plusch(sudoku)) return sudoku;
-		}
-		sudoku[j][i--] = 0;
-		if (i < 0) i = 8, j--;
-		return 0;
-	}
-	return plusch(sudoku);
+	let posNum = [1, 2, 3, 4, 5, 6 ,7 ,8, 9].filter(elem => !impsNum.includes(elem));
+	for (sudoku[row][col] of posNum) if (solveSudoku(sudoku, col + 1, row)) return sudoku;
+	return sudoku[row][col] = 0;
 }
